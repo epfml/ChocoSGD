b5fd08f136c568d99e7f5cf6cb7ead3cce3746fd
IamTao <itamtao@gmail.com>
2020-04-03 12:57:30 +0200

    update the code in the public repo (remove useless optimizer).

--- dl_code/pcode/create_optimizer.py
+++ dl_code/pcode/create_optimizer.py
@@ -5,17 +5,9 @@ from pcode.optim.dgc import DGC
 from pcode.optim.parallel_choco import ParallelCHOCO
 from pcode.optim.parallel_choco_v import ParallelCHOCO_V
 from pcode.optim.ef_sign_sgd import EF_SignSGD
-from pcode.optim.others.sign_sgd import SignSGD
-from pcode.optim.others.local_sign_sgd import Local_SignSGD
-from pcode.optim.others.local_sgd import LocalSGD
 from pcode.optim.dcd_psgd import DCD_PSGD
 from pcode.optim.ecd_psgd import ECD_PSGD
 from pcode.optim.deep_squeeze import DeepSqueeze
-from pcode.optim.others.local_ef_sign_sgd_v import Local_EFSignSGD_V
-from pcode.optim.others.local_sign_sgd_v import Local_SignSGD_V
-from pcode.optim.others.sign_sgd_v import SignSGD_V
-from pcode.optim.others.adam import Adam
-from pcode.optim.others.local_adam import LocalAdam
 
 
 def define_optimizer(conf, model):
@@ -46,24 +38,8 @@ def define_optimizer(conf, model):
         optim_class = ParallelCHOCO_V
     elif conf.optimizer == "ef_sign_sgd":
         optim_class = EF_SignSGD
-    elif conf.optimizer == "sign_sgd":
-        optim_class = SignSGD
-    elif conf.optimizer == "local_sign_sgd":
-        optim_class = Local_SignSGD
-    elif conf.optimizer == "local_sgd":
-        optim_class = LocalSGD
     elif conf.optimizer == "deep_squeeze":
         optim_class = DeepSqueeze
-    elif conf.optimizer == "sign_sgd_v":
-        optim_class = SignSGD_V
-    elif conf.optimizer == "local_sign_sgd_v":
-        optim_class = Local_SignSGD_V
-    elif conf.optimizer == "local_ef_sign_sgd_v":
-        optim_class = Local_EFSignSGD_V
-    elif conf.optimizer == "adam":
-        optim_class = Adam
-    elif conf.optimizer == "local_adam":
-        optim_class = LocalAdam
     else:
         raise NotImplementedError
 
